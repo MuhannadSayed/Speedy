8f167dd978727fc0c6008bd3d3f2909fdb6b9409
muhannadsayedabdullah <symohannad@gmail.com>
2021-02-06 00:29:59 +0100

    cart class

---
+++ src/TireMall/Showroom/Cart.java
@@ -0,0 +1,10 @@
+package TireMall.Showroom;
+
+import java.util.ArrayList;
+import java.util.Stack;
+import java.util.stream.Stream;
+
+public class Cart {
+
+    final ArrayList<CartItem> items = new ArrayList<>();
+}
